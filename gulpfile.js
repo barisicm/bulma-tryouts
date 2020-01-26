a43e3475e52c8e506e14e54630f2a3b4fa2aabd3
Marin <barisic.m@outlook.com>
2020-01-26 22:08:21 +0100

    Bulma tryouts initial commit

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+// Define browser sync
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir:'./dist'
+        }
+    });
+
+    gulp.watch('*.html').on('change', reload);
+    gulp.watch('css/*.css').on('change', reload);
+});
